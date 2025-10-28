91bb1d711b833667e162b9af47ef49af2d91f9db
Mevans <mrmagnus3@gmail.com>
2025-10-28 18:43:07 +0000

    Add tex test and bibliography file

--- graph_model_prototype_smart_ptr.cpp
+++ graph_model_prototype_smart_ptr.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <stdlib.h>
 
+#define WIDTH 10
+#define HEIGHT 10
 
 struct Vertex {
         char state;
@@ -36,42 +38,80 @@ std::ostream& operator<<(std::ostream& os, const Vertex& vOut)
         return os;
 }
 
+
+bool connected(const std::shared_ptr<Vertex>& v1,
+               const std::shared_ptr<Vertex>& v2)
+{
+        for (int i = 0; i < v1->edges.size(); i++) {
+                if (v1->edges[i].lock() == v2) return true;
+        }
+        return false;
+}
+
+// TODO make this nicer
+void draw_graph(std::ostream& os,
+                const std::vector<std::vector<std::shared_ptr<Vertex>>>& graph)
+{
+        for (int i = 0; i < graph.size()-1; i++) {
+                std::string bufferline = "";
+                for (int j = 0; j < graph[i].size()-1; j++) {
+                        os << graph[i][j]->state;
+                        if (connected(graph[i][j], graph[i][j+1])) os << "-";
+                        else os << " ";
+                        if (connected(graph[i][j], graph[i+1][j])) bufferline += "|";
+                        else bufferline += " ";
+                        bufferline += " ";
+                }
+                os << graph[i][graph[i].size()-1]->state << std::endl;
+                if (connected(graph[i][graph[i].size()-1], graph[i+1][graph[i].size()-1])) bufferline += "|";
+                else bufferline += " ";
+                os << bufferline << std::endl;
+        }
+        for (int j = 0; j < graph[graph.size()-1].size()-1; j++) {
+                os << graph[graph.size()-1][j]->state;
+                if (connected(graph[graph.size()-1][j], graph[graph.size()-1][j+1])) os << "-";
+                else os << " ";
+        }
+        os << graph[graph.size()-1][graph[graph.size()-1].size()-1]->state;
+}
+
+
 int main()
 {
-        std::vector<std::vector<std::shared_ptr<Vertex>>> lattice(4);
+        std::vector<std::vector<std::shared_ptr<Vertex>>> lattice(HEIGHT);
         
         // Allocate all the shared pointers to vertices
-        for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
                         std::shared_ptr<Vertex> vtemp(new Vertex('S')); 
                         lattice[i].push_back(vtemp);
                 }
         }
 
         // Add edges to vertices to make a square lattice
-        for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < HEIGHT-1; i++) {
+                for (int j = 0; j < WIDTH; j++) {
                         std::weak_ptr<Vertex> neighbourTemp (lattice[i+1][j]);
                         lattice[i][j]->edges.push_back(neighbourTemp);
                 }
         }
 
-        for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH-1; j++) {
                         std::weak_ptr<Vertex> neighbourTemp (lattice[i][j+1]);
                         lattice[i][j]->edges.push_back(neighbourTemp);
                 }
         }
 
-        for (int i = 1; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
+        for (int i = 1; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
                         std::weak_ptr<Vertex> neighbourTemp (lattice[i-1][j]);
                         lattice[i][j]->edges.push_back(neighbourTemp);
                 }
         }
 
-        for (int i = 0; i < 4; i++) {
-                for (int j = 1; j < 4; j++) {
+        for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 1; j < WIDTH; j++) {
                         std::weak_ptr<Vertex> neighbourTemp (lattice[i][j-1]);
                         lattice[i][j]->edges.push_back(neighbourTemp);
                 }
@@ -79,13 +119,16 @@ int main()
 
 
         // Output all vertex info
-        for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
                         std::cout << "Vertex (" << i << ", " << j << "): "
                                   << lattice[i][j] << std::endl;
                         std::cout << *lattice[i][j];
                 }
         }
+
+        draw_graph(std::cout, lattice);
+
 
         /* leftover deletion code from raw pointer implementation
         for (int i = 0; i < 4; i++) {

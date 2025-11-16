fb7690e80a0869578f3b6e0f3f7e66d025cf9559
Mevans <mrmagnus3@gmail.com>
2025-11-16 16:49:33 +0000

    Add SI modelling to python graph

--- lattice_test.py
+++ lattice_test.py
@@ -4,24 +4,35 @@ from matplotlib import pyplot as plt
 import networkx as nx
 
 
-SIZE = 100  # Side length of lattice
+SIZE = 20  # Side length of lattice
+prob = 0.5  # Probability of edge existence
+
+state_dict = {0: 'H', 1: 'S', 2: 'I'}
+colour_dict = {'H': "black", 'S': "green", 'I': "red"}
 
 
-def draw_graph(M):
+def draw_graph(M, sts):
     '''
     Draws the graph given in an adjacency matrix
-    Params: M, an adjacency matrix of a graph
-    Note: this will not necessarily draw a lattice graph "sensibly"
+    Params:
+    M: An adjacency matrix of a graph
+    sts: A dictionary of nodes with states of the graph
     '''
     rows, cols, vals = sp.sparse.find(M)
     edges = zip(rows.tolist(), cols.tolist())
     gr = nx.Graph()
+    toadd = [i for i in range(SIZE**2)]
+    gr.add_nodes_from(toadd)
     gr.add_edges_from(edges)
     offset = np.array([0, 0])
     pos = nx.circular_layout(gr)
+    nx.set_node_attributes(gr, sts, name="state")
+    node_colour = [colour_dict[nx.get_node_attributes(gr, "state")[i]]
+                   for i in range(SIZE**2)]
     for i in range(SIZE**2):
         pos[i] = offset + np.array([i % SIZE, (i//SIZE)])
-    nx.draw(gr, pos, node_size=20)
+    plt.figure(figsize=(7, 7))
+    nx.draw(gr, pos, node_size=20, node_color=node_colour)
     plt.show()
 
 
@@ -38,7 +49,6 @@ A = sp.sparse.kron(offdi, I, format='csr') \
 
 
 # Randomly remove edges
-prob = 0.5
 rowsout, colsout, vals = sp.sparse.find(sp.sparse.triu(A))  # Only upper tri
 edgesout = [edge for edge in zip(rowsout.tolist(), colsout.tolist())
             if prob < np.random.rand()]
@@ -48,4 +58,37 @@ for edge in edgesout:
 A.eliminate_zeros()
 
 
-draw_graph(A)
+# Initialise SI vectors
+suscepts = np.ones(SIZE**2)
+infects = np.zeros(SIZE**2)
+
+
+# Begin with an infected
+infects[SIZE**2//2 + SIZE//2] = 1
+suscepts[SIZE**2//2 + SIZE//2] = 0
+
+
+# Naive time-step iteration
+while 1 in suscepts:
+    # Ensure exit after no change
+    oldinfects = np.copy(infects)
+    # Set up state vectors to display correctly
+    statevec = suscepts + 2*infects
+    states = {x: state_dict[statevec[x]] for x in range(SIZE**2)}
+    # Infect neighbours
+    infects = infects + ((A @ infects) * suscepts)
+    # These style of expression reset all entries to 1 or 0
+    infects[infects > 1] = 1
+    infects[infects < 0] = 0
+    # Make sure vectors sum to 1
+    suscepts = suscepts - infects
+    suscepts[suscepts < 0] = 0
+    suscepts[suscepts > 1] = 1
+    # Draw the graph
+    draw_graph(A, states)
+    # break loop if no updates occurred
+    if (np.array_equal(infects, oldinfects)):
+        break
+statevec = suscepts + 2*infects
+states = {x: state_dict[statevec[x]] for x in range(SIZE**2)}
+draw_graph(A, states)
